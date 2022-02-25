438abe345e8796e840d0952740fee8eb23c935bb
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-02-25 15:18:08 +0200

    Start

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-const bootstrap = require('bootstrap')
+
 gulp.task('server', function() {
 
     browserSync({
